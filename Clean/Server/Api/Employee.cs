06683bebbcba83c9d8affd312942e0e3ab7ef932
Kal Kallevig <kal@ecbridge.com>
2020-06-12 19:03:08 -0700

    initial commit

---
+++ Clean/Server/Api/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Clean.Server.Api
+{
+    public partial class Employee
+    {
+        public long EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
---
+++ Clean/Server/Api/ManagementContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clean.Server.Api
+{
+    public partial class ManagementContext : DbContext
+    {
+        public ManagementContext()
+        {
+        }
+
+        public ManagementContext(DbContextOptions<ManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(Clean.Server.Startup.Connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Location)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
